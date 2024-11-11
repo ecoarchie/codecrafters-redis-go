e5ff46cd7b0ca194bf619394139842c18e579d2e
ecoarchie <eco.artur@gmail.com>
2024-11-11 16:36:15 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -22,17 +22,30 @@ func main() {
 	}
 	defer l.Close()
 
+	r := NewRedis()
+
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConn(conn)
+		go r.handleConn(conn)
 	}
 }
 
-func handleConn(conn net.Conn) {
+type Redis struct {
+	data map[string]string
+}
+
+func NewRedis() *Redis {
+	data := make(map[string]string)
+	return &Redis{
+		data: data,
+	}
+}
+
+func (r *Redis) handleConn(conn net.Conn) {
 	defer conn.Close()
 	for {
 		buf := make([]byte, 1024)
@@ -42,12 +55,12 @@ func handleConn(conn net.Conn) {
 		}
 
 		// fmt.Println("Received data", string(buf[:n]))
-		reply := parseRESParray(string(buf[:n]))
+		reply := r.parseRESParray(string(buf[:n]))
 		conn.Write([]byte(reply))
 	}
 }
 
-func parseRESParray(s string) string {
+func (r *Redis) parseRESParray(s string) string {
 	arr := strings.Split(s, "\r\n")
 	var reply string
 
@@ -62,6 +75,15 @@ func parseRESParray(s string) string {
 	case "ping":
 		fmt.Println("entering PING")
 		reply = "$4\r\nPONG\r\n"
+	case "set":
+		key := arr[4]
+		val := arr[6]
+		r.data[key] = val
+		reply = "+OK\r\n"
+	case "get":
+		key := arr[4]
+		replyVal := r.data[key]
+		reply = fmt.Sprintf("$%d\r\n%s\r\n", len(replyVal), replyVal)
 	}
 	return reply
 }
